3a4685f1a721c7af3962f7ea7601480650ee3b02
zjmorgan <zjmorgan@mtu.edu>
2025-08-03 14:17:37 -0400

    Tool tips

--- src/NeuXtalViz/views/base_view.py
+++ src/NeuXtalViz/views/base_view.py
@@ -37,22 +37,28 @@ class NeuXtalVizWidget(QWidget):
 
         self.proj_box = QCheckBox("Parallel Projection", self)
         self.proj_box.setChecked(True)
+        self.proj_box.setToolTip("Toggle parallel projection for the 3D view.")
         self.proj_box.clicked.connect(self.change_projection)
 
         self.reset_button = QPushButton("Reset View", self)
+        self.reset_button.setToolTip("Reset the view to the default orientation.")
         self.reset_button.clicked.connect(self.reset_view)
 
         self.camera_button = QPushButton("Reset Camera", self)
+        self.camera_button.setToolTip("Reset the camera position.")
         self.camera_button.clicked.connect(self.reset_camera)
 
         self.recip_box = QCheckBox("Reciprocal Lattice", self)
         self.recip_box.setChecked(True)
+        self.recip_box.setToolTip("Show or hide reciprocal lattice vectors.")
 
         self.axes_box = QCheckBox("Show Axes", self)
         self.axes_box.setChecked(True)
+        self.axes_box.setToolTip("Show or hide the coordinate axes in the plot.")
         self.axes_box.clicked.connect(self.show_axes)
 
         self.save_button = QPushButton("Save Screenshot", self)
+        self.save_button.setToolTip("Save a screenshot of the current 3D view.")
 
         self.frame = QFrame()
 
@@ -113,11 +119,13 @@ class NeuXtalVizWidget(QWidget):
         self.view_combo = QComboBox(self)
         self.view_combo.addItem("[hkl]")
         self.view_combo.addItem("[uvw]")
+        self.view_combo.setToolTip("Select axis notation for view direction.")
         self.view_combo.currentIndexChanged.connect(self.update_labels)
 
         self.viewup_combo = QComboBox(self)
         self.viewup_combo.addItem("[hkl]")
         self.viewup_combo.addItem("[uvw]")
+        self.viewup_combo.setToolTip("Select axis notation for up direction.")
         self.viewup_combo.currentIndexChanged.connect(self.update_labels)
 
         notation = QDoubleValidator.StandardNotation
@@ -131,6 +139,9 @@ class NeuXtalVizWidget(QWidget):
         self.axis1_line.setValidator(validator)
         self.axis2_line.setValidator(validator)
         self.axis3_line.setValidator(validator)
+        self.axis1_line.setToolTip("First component of the view direction (e.g., h or u)")
+        self.axis2_line.setToolTip("Second component of the view direction (e.g., k or v)")
+        self.axis3_line.setToolTip("Third component of the view direction (e.g., l or w)")
 
         self.axis1_label = QLabel("h", self)
         self.axis2_label = QLabel("k", self)
@@ -143,37 +154,46 @@ class NeuXtalVizWidget(QWidget):
         self.axisup1_line.setValidator(validator)
         self.axisup2_line.setValidator(validator)
         self.axisup3_line.setValidator(validator)
+        self.axisup1_line.setToolTip("First component of the up direction (e.g., h or u)")
+        self.axisup2_line.setToolTip("Second component of the up direction (e.g., k or v)")
+        self.axisup3_line.setToolTip("Third component of the up direction (e.g., l or w)")
 
         self.axisup1_label = QLabel("h", self)
         self.axisup2_label = QLabel("k", self)
         self.axisup3_label = QLabel("l", self)
 
         self.manual_button = QPushButton("View Axis", self)
+        self.manual_button.setToolTip("Set the view direction using the specified axis components.")
         self.manualup_button = QPushButton("View Up Axis", self)
+        self.manualup_button.setToolTip("Set the up direction using the specified axis components.")
 
         self.px_button = QPushButton("+Qx", self)
+        self.px_button.setToolTip("View along the +Qx direction.")
         self.py_button = QPushButton("+Qy", self)
+        self.py_button.setToolTip("View along the +Qy direction.")
         self.pz_button = QPushButton("+Qz", self)
+        self.pz_button.setToolTip("View along the +Qz direction.")
 
         self.mx_button = QPushButton("-Qx", self)
+        self.mx_button.setToolTip("View along the -Qx direction.")
         self.my_button = QPushButton("-Qy", self)
+        self.my_button.setToolTip("View along the -Qy direction.")
         self.mz_button = QPushButton("-Qz", self)
-
-        self.px_button.clicked.connect(self.view_yz)
-        self.py_button.clicked.connect(self.view_zx)
-        self.pz_button.clicked.connect(self.view_xy)
-
-        self.mx_button.clicked.connect(self.view_zy)
-        self.my_button.clicked.connect(self.view_xz)
-        self.mz_button.clicked.connect(self.view_yx)
+        self.mz_button.setToolTip("View along the -Qz direction.")
 
         self.a_star_button = QPushButton("a*", self)
+        self.a_star_button.setToolTip("Align view along the a* reciprocal axis.")
         self.b_star_button = QPushButton("b*", self)
+        self.b_star_button.setToolTip("Align view along the b* reciprocal axis.")
         self.c_star_button = QPushButton("c*", self)
+        self.c_star_button.setToolTip("Align view along the c* reciprocal axis.")
 
         self.a_button = QPushButton("a", self)
+        self.a_button.setToolTip("Align view along the a real axis.")
         self.b_button = QPushButton("b", self)
+        self.b_button.setToolTip("Align view along the b real axis.")
         self.c_button = QPushButton("c", self)
+        self.c_button.setToolTip("Align view along the c real axis.")
 
         directions_layout = QGridLayout()
         manual_layout = QGridLayout()
